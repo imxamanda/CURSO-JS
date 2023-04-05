2ca3eaa41d266e2e962e0d6a6a821b6f082a9773
imxamanda <amandavieiraperes.12@gmail.com>
2023-04-05 08:46:56 -0300

    criando uma lista

---
+++ main.js
@@ -0,0 +1,10 @@
+//CRIANDO UMA LISTA
+
+const names = ["Felipe", "João", "Julia", 10, false];
+//Os valores da lista podem ser de qualquer tipo;
+
+const joao = names[1];
+//começa a contagem do 0
+console.log(joao)
+
+
