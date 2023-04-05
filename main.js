6692d19fc15793374280548e27456e1a78fb1060
imxamanda <amandavieiraperes.12@gmail.com>
2023-04-05 10:46:56 -0300

    filter

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-//Entendendo o Map
+//ENTENDENDO O MAP
 const numbers = [1, 2, 3, 4, 5];
 
 const numberMultipliedByTwo = numbers.map(function(number){
-     return number * 2;
+    return number * 2;
 })
 
-console.log(numberMultipliedByTwo);
+//console.log(numberMultipliedByTwo); -comentario para poder rodar o filter
 
+//ENTENDENDO O FILTER
+ 
+const ages = [8,13 ,27, 30, 22, 40]
+
+const evenAges = ages.filter(function(age){
+    return age % 2 === 0;
+})
+
+console.log(evenAges);
