1941cdcb34044fe4ca8b393bf0205d70e77b11d5
imxamanda <amandavieiraperes.12@gmail.com>
2023-04-05 11:55:01 -0300

    falta finalizar jason to variavel

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-//UTILIZANDO LISTAS COM OBJETOS
+//convertendo variavel em json
 
+//const todos = [
+//    {
+//        id:1,
+//        description: 'Estudar programação',
+//        isCompleted: false,
+//    },
+//    {
+//        id:2,
+//        description: 'Ler',
+//        isCompleted: true,
+//    },
+//    {
+//        id:3,
+//        description: 'Treinar',
+//        isCompleted: true,
+//    },
+//];
+
+//const todosJSON = JSON.stringify(todos);
+
+//console.log(todosJSON[1]);
+
+//CONVERTENDO JSON PARA UMA VARIÁVEL
 const todos = [
     {
         id:1,
@@ -18,6 +41,4 @@ const todos = [
     },
 ];
 
-const descriptionOfLastTodo = todos [2].description;
-
-console.log(descriptionOfLastTodo);
+const todosJSON = JSON.stringify(todos);
