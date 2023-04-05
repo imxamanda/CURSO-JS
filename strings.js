134ced69e60be87a0b74b9d81735d2edf5e96a03
imxamanda <amandavieiraperes.12@gmail.com>
2023-04-05 08:30:29 -0300

    Listas

---
+++ strings.js
@@ -0,0 +1,43 @@
+//VARIAVEIS
+//var message = 'Hello World!'
+
+//alert(message)
+
+//const messege = 'Hello WOrld!' - não dá p alterar a mensagem depois
+
+//let messege = 'Hello WOrld!' 
+//message = 'ola mundo' - não dá erro, pos é possível re-assinalar
+
+// STRINGS
+//const message = 'Hello World!'
+//console.log(message.length) 
+
+//const firstName = 'Amanda'
+//const lastName = 'Peres'
+
+//const names = 'Julia,João,José'
+
+//console.log("Meu nome é " + firstName + " " + lastName)
+//console.log (`Meu nome é  ${firstName.toUpperCase()} ${lastName.toLowerCase()}`);
+
+//console.log(names.split(","));
+//console.log(names.split("")); //soletra
+
+//NUMBERS
+//const number = 5;
+
+//console.log(number.toString());
+//console.log(typeof number.toString())
+
+//BOOLEANS
+//false
+//true
+
+//ex
+//console.log(2 == 2)
+
+//NULL & Undefined
+
+//const X = null;
+
+//const y = undefined;
