e385aeb0eea0e1f583d3235cea27e0df0a23f436
imxamanda <amandavieiraperes.12@gmail.com>
2023-04-10 14:10:42 -0300

    json

---
+++ json.js
@@ -0,0 +1,44 @@
+//convertendo variavel em json
+
+//const todos = [
+//    {
+//        id:1,
+//        description: 'Estudar programação',
+//        isCompleted: false,
+//    },
+//    {
+//        id:2,
+//        description: 'Ler',
+//        isCompleted: true,
+//    },
+//    {
+//        id:3,
+//        description: 'Treinar',
+//        isCompleted: true,
+//    },
+//];
+
+//const todosJSON = JSON.stringify(todos);
+
+//console.log(todosJSON[1]);
+
+//CONVERTENDO JSON PARA UMA VARIÁVEL
+const todos = [
+    {
+        id:1,
+        description: 'Estudar programação',
+        isCompleted: false,
+    },
+    {
+        id:2,
+        description: 'Ler',
+        isCompleted: true,
+    },
+    {
+        id:3,
+        description: 'Treinar',
+        isCompleted: true,
+    },
+];
+
+const todosJSON = JSON.stringify(todos);
