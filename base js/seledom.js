ef6cdf1e54b1adfeaad3450a40a13b251d93688f
imxamanda <amandavieiraperes.12@gmail.com>
2023-04-12 11:36:34 -0300

    dom

---
+++ base js/seledom.js
@@ -0,0 +1,22 @@
+//SELECIONANDO ELEMENTOS DOM 
+//(selecionar apenas um elemento)
+//const addUserText = document.getElementById('add-user');
+
+//addUserText.innerText = "Adicionar usuário"
+
+//ENETNDENDO QUERYSELECTOR
+//(selecionar apenas um elemento)
+//const addUserText = document.querySelector('#add-user');  
+//addUserText.textContent = "Adicionar usuário"
+//const myForm = document.querySelector(".container #my-form");
+//const myForm = document.querySelector(".item");
+//console.log(myForm);
+
+//SELECIONANDO MÚLTIPLOS ELEMENTOS
+//const allItems = document.querySelectorAll(".items .item"); (ESSE É O RECOMENDADO)
+
+//não são recomendados
+//const allItems = document.getElementsByClassName("item");
+//const allItems = document.getElementsByTagName("li");
+
+//console.log(allItems);
