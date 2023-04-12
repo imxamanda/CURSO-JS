22d77f16f5c0e78f728265497fc66129686337d6
imxamanda <amandavieiraperes.12@gmail.com>
2023-04-12 09:57:44 -0300

    poo2

---
+++ base js/POO2.js
@@ -0,0 +1,25 @@
+//Entendendo Herança
+class Animal{
+    constructor(name){
+        this.name = name;
+    }
+    speak() {
+        console.log(`${this.name} made some noise!`);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name) {
+        super(name);
+    }
+    
+    speak(){
+        console.log(`${this.name} barked!`);
+    }
+}
+
+const animal = new Animal("Simba");
+const dog = new Dog("Bob");
+
+animal.speak();
+dog.speak();
